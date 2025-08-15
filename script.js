f14417aa928c72fc2597249b6d5d534ea4200a58
Sasha Tkachuk <sasha.tka4uk2011@gmail.com>
2025-08-15 20:23:42 +0300

    Added mobile navigation

---
+++ script.js
@@ -0,0 +1,10 @@
+/*******************\
+    Some JS code for mobile navigation 
+\*******************/
+
+const mobileBtn = document.querySelector(".header__mobile");
+const mobileNavigationBox = document.querySelector(".header__nav");
+
+mobileBtn.addEventListener("click", function () {
+  mobileNavigationBox.classList.toggle("open");
+});
